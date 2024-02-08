0d9056bf621a7a3dd52222d6de784d694c0c9d1e
abhishek-bhangalia-busy <158018556+abhishek-bhangalia-busy@users.noreply.github.com>
2024-02-08 11:00:49 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,72 +4,68 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
-	"strconv"
 )
 
-var inp string = `{
-"name" : "Tolexo Online Pvt. Ltd",
-"age_in_years" : 8.5,
-"origin" : "Noida",
-"head_office" : "Noida, Uttar Pradesh",
-"address" : [
-{
-"street" : "91 Springboard",
-"landmark" : "Axis Bank",
-"city" : "Noida",
-"pincode" : 201301,
-"state" : "Uttar Pradesh"
-},
-{
-"street" : "91 Springboard",
-"landmark" : "Axis Bank",
-"city" : "Noida",
-"pincode" : 201301,
-"state" : "Uttar Pradesh"
-}
-],
-"sponsers" : {
-"name" : "One"
-},
-"revenue" : "19.8 million$",
-"no_of_employee" : 630,
-"str_text" : ["one","two"],
-"int_text" : [1,3,4]
-}`
+func printJSON(d interface{}, idn string) {
+	v := reflect.ValueOf(d)
 
-func printSliceItems(s []interface{}) {
-	for i, v := range s {
-		rObjVal := reflect.ValueOf(v)
-		checkNestedTypes(rObjVal, strconv.Itoa(i))
-	}
-}
-
-func checkNestedTypes(rObjVal reflect.Value, k string) {
-	switch rObjVal.Kind() {
+	switch v.Kind() {
 	case reflect.Map:
-		printJSON(rObjVal.Interface().(map[string]interface{}))
+		fmt.Printf("{ (type : %v, kind : %v)", v.Type(), v.Kind())
+		for _, k := range v.MapKeys() {
+			fmt.Printf("\n%v\t%v : ", idn, k)
+			printJSON(v.MapIndex(k).Interface(), idn+"\t")
+		}
+		fmt.Printf("\n    %v}", idn)
 	case reflect.Slice:
-		printSliceItems(rObjVal.Interface().([]interface{}))
-	default:
-		rObjTyp := reflect.TypeOf(rObjVal.Interface())
-		fmt.Printf("key : %v, value : %v, type : %v, kind : %v\n", k, rObjVal, rObjTyp, rObjVal.Kind())
-	}
-}
+		fmt.Printf("[ (type : %v, kind : %v)", v.Type(), v.Kind())
+		for i := 0; i < v.Len(); i++ {
+			fmt.Printf("\n%v\t%v : ", idn, i)
+			printJSON(v.Index(i).Interface(), idn+"\t")
 
-func printJSON(mp map[string]interface{}) {
-	for k, v := range mp {
-		rObjVal := reflect.ValueOf(v)
-		checkNestedTypes(rObjVal, k)
+		}
+		fmt.Printf("\n    %v]", idn)
+	default:
+		fmt.Printf("%v, (type : %v, kind : %v)", v.Interface(), v.Type(), v.Kind())
 	}
 }
 
 func main() {
-	var mp = make(map[string]interface{})
+	var inp string = `{
+		"name" : "Tolexo Online Pvt. Ltd",
+		"age_in_years" : 8.5,
+		"origin" : "Noida",
+		"head_office" : "Noida, Uttar Pradesh",
+		"address" : [
+			{
+				"street" : "91 Springboard",
+				"landmark" : "Axis Bank",
+				"city" : "Noida",
+				"pincode" : 201301,
+				"state" : "Uttar Pradesh"
+			},
+			{
+				"street" : "91 Springboard",
+				"landmark" : "Axis Bank",
+				"city" : "Noida",
+				"pincode" : 201301,
+				"state" : "Uttar Pradesh"
+			}
+		],
+		"sponsers" : {
+			"name" : "One"
+		},
+		"revenue" : "19.8 million$",
+		"no_of_employee" : 630,
+		"str_text" : ["one","two"],
+		"int_text" : [1,3,4]
+	}`
+	var mp interface{}
 	err := json.Unmarshal([]byte(inp), &mp)
 
 	if err != nil {
 		panic(err)
 	}
 
-	printJSON(mp)
+	printJSON(mp, "")
 }
